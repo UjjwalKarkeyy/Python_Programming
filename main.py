07eb9913770292c3549d0293f131457d3b925d71
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2024-12-02 10:05:58 +0545

    week-7_2nd_commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import math_utils
 from mathematics.trigonometry import cosine, sine
 
@@ -9,3 +10,19 @@ from mathematics import trigonometry
 cosine_of_45 = mathematics.trigonometry.cosine(45)
 trigonometry.cosine(45)
 print(cosine_of_45)
+
+# configure logging (log to console by default)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# different log levels
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
+
+try:
+    100/0
+
+except ZeroDivisionError as e:
+    logging.error(f"Error: {e}!")

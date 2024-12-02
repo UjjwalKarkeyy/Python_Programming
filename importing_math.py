34c54b173280780497de6a7e1549aa3cbf564ebb
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2024-12-02 08:48:12 +0545

    week-7_first_class

--- importing_math.py
+++ importing_math.py
@@ -1,5 +1,5 @@
 import mathematics
-from mathematics import math_utils
+from . import math_utils
 
 result = math_utils.add(5,6)
 print(result)
---
+++ main.py
@@ -0,0 +1,11 @@
+import math_utils
+from mathematics.trigonometry import cosine, sine
+
+result = math_utils.add(1,7)
+print(result)
+
+import mathematics
+from mathematics import trigonometry
+cosine_of_45 = mathematics.trigonometry.cosine(45)
+trigonometry.cosine(45)
+print(cosine_of_45)
